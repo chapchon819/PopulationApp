c24e586835402686644868fa73089f32a9351d71
chapchon819 <l000merci.819@gmail.com>
2024-10-09 00:06:51 +0900

    🩹 EslintConfigPrettierをimport

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 
 export default [
