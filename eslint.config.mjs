165d0759c478e554cf220e1cb5b4e196814be175
chapchon819 <l000merci.819@gmail.com>
2024-10-08 23:56:52 +0900

    ✨ Add:Eslint・prettier導入

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      "no-console": "warn",
+      "@typescript-eslint/no-explicit-any": "error"
+    },
+    settings: {
+      react: {
+        version: "18.3.1",
+      }
+    }
+  },
+  eslintConfigPrettier,
+];
