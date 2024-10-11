f334f83b57ebf3d92d9fd6902cead7cf18ec191f
chapchon819 <l000merci.819@gmail.com>
2024-10-11 22:53:45 +0900

    📦️ Add:Highchartsをインストールした

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import * as Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
 
-function App() {
-  return <h1 className="text-3xl font-bold underline decoration-green-300">Hello world!</h1>;
-}
+// The wrapper exports only a default component that at the same time is a
+// namespace for the related Props interface (HighchartsReact.Props) and
+// RefObject interface (HighchartsReact.RefObject). All other interfaces
+// like Options come from the Highcharts module itself.
+
+const options: Highcharts.Options = {
+    title: {
+        text: 'My chart'
+    },
+    series: [{
+        type: 'line',
+        data: [1, 2, 3]
+    }]
+};
+
+const App = (props: HighchartsReact.Props) => {
+  const chartComponentRef = useRef<HighchartsReact.RefObject>(null);
+
+  return (
+    <HighchartsReact
+      highcharts={Highcharts}
+      options={options}
+      ref={chartComponentRef}
+      {...props}
+    />
+  );
+};
 
 export default App;
